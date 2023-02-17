13f6b09dce168b1182880d12c38275e4bf977d64
wnsgus313 <37920523+wnsgus313@users.noreply.github.com>
2023-02-17 19:53:11 +0900

    [Silver III] Title: N과 M (3), Time: 352 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+bool visited[9];
+int arr[9];
+
+void go(int cnt){
+  if(cnt == M){
+    for(int i=0; i<M; i++){
+      cout << arr[i] << ' ';
+    }
+    cout << '\n';
+    
+    return;
+  }
+
+  for(int i=1; i<=N; i++){
+      arr[cnt] = i;
+      visited[i] = 1;
+      go(cnt + 1);
+      visited[i] = 0;
+  }
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+  cin >> N >> M;
+  go(0);
+}
