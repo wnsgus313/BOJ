659c5f701e8cc471c3a753cd0f5699caed6794af
wnsgus313 <37920523+wnsgus313@users.noreply.github.com>
2023-04-28 18:34:29 +0900

    [unrated] Title: 디펜스 게임, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.cpp
@@ -0,0 +1,40 @@
+// 41분
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(int n, int k, vector<int> enemy) {
+    int answer = k;
+    int s = enemy.size(); // 적군의 길이
+    priority_queue<int> pq;
+    
+    // k번까지 min heap에 넣기
+    for(int i=0; i<k; i++){
+        pq.push(-enemy[i]);
+    }
+
+    // 한개씩 빼면서 비교
+    while(1){
+        if(answer >= s) break; // 적군의 길이가 현재 참조할 enemy 순서보다 작거나 같으면 종료
+        
+        pq.push(-enemy[answer]); // 새로운 적을 넣기
+        int e = -pq.top(); // 가장 적은 적군의 수
+        pq.pop();
+
+        // 싸울 수 있으면 추가 
+        if(n - e >= 0){
+            answer++; // 값 증가
+            
+            n -= e; // 싸운 적군만큼 차감
+        }
+        else{
+            break;
+        }
+    }
+    
+    if(answer >= s) answer = s; // 정답이 초과되면 enemy의 길이로 변경
+    
+    return answer;
+}
