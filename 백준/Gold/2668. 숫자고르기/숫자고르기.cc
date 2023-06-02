9f817befccfa3e6f21ab7757fe15cb6a7bb8aa97
wnsgus313 <37920523+wnsgus313@users.noreply.github.com>
2023-06-02 22:23:15 +0900

    [Gold V] Title: 숫자고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.cc
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.cc
@@ -5,29 +5,25 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <cstring>
 using namespace std;
 
 priority_queue<int> pq; // 뽑힌 정수
-int arr[2][102]; // idx, 숫자
+int arr[102]; // idx, 숫자
 int visited[102];
 vector<int> v;
-vector<int> res;
 void Find(int start, int from){
   // 사이클이면 다 집어 넣기
   if(start == from){
-    for(auto au : v){
-      pq.push(-au);
-    }
-    res = v;
+    pq.push(-start);
   }
   // 이미 방문했으면 종료
   if(visited[from]) return;
   
   v.push_back(from);
   visited[from] = 1;
-  Find(start, arr[1][from]);
+  Find(start, arr[from]);
   v.pop_back();
-  visited[from] = 0;
 }
 int main()
 {
@@ -36,28 +32,16 @@ int main()
 
   cin >> N;
   for(int i=1; i<=N; i++){
-    arr[0][i] = i;
-    cin >> arr[1][i];
-
-    // 위 아래가 같으면 깍두기
-    if(i == arr[1][i]) {
-      visited[i] = 1;
-      pq.push(-i);
-    }
+    cin >> arr[i];
   }
 
   // 깍두기를 제외하고 사이클이 이뤄지면 가능
   for(int i=1; i<=N; i++){
-    for(auto au : res){
-      visited[au] = 1;
-    }
-    res.clear();
-    if(visited[i] == 1) continue;
-
+    memset(visited, 0, sizeof(visited));
+    
     v.push_back(i);
-    visited[i] = 0;
-    Find(i, arr[1][i]);
-    visited[i] = 0;
+    visited[i] = 1;
+    Find(i, arr[i]);
     v.pop_back();
   }
 
